5d4ec196df6053b8c1a078b2d22f55906e11e65d
RyanHopkins7 <ryan.hopkins@students.makeschool.com>
2019-10-09 17:14:49 -0700

    Add add-new route

--- app.py
+++ app.py
@@ -1,19 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
 client = MongoClient()
 db = client.Marketplace
-playlists = db.marketplace
+marketplace = db.marketplace
 
 @app.route('/')
 def index():
+    print(marketplace.find())
+    print(marketplace.find_one({'_id': ObjectId('5d9e013cf14aea0e5c07544d')}))
     return render_template('index.html')
 
 @app.route('/items/new')
 def new_item():
     return render_template('new-item.html')
+
+@app.route('/items/add-new', methods=['POST'])
+def add_new_item():
+    new_item = {
+        'title': request.form.get('title'),
+        'description': request.form.get('description'),
+        'price': request.form.get('price'),
+        'image': request.form.get('picture')
+    }
+    product_id = marketplace.insert_one(new_item).inserted_id
+    print(product_id)
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
