e7a503c5b5bce096ba2100dc74464ccef3730cb0
RyanHopkins7 <ryan.hopkins@students.makeschool.com>
2019-10-10 09:37:28 -0700

    Fix display_products

--- static/display_products.js
+++ static/display_products.js
@@ -6,21 +6,19 @@ if (productContainer) {
     let numProducts = products.length;
 
     // Display only 3 products per row
-    if (numProducts > 3) {
-        for (let index = 0; index < numProducts; index++) {
-            if (index % 3 == 0) {
-                currentRow = document.createElement('DIV');
-                currentRow.className += 'row';
-                container.appendChild(currentRow);
-            }
-
-            // Limit length of text in description on home page
-            let productDescription = products[0].getElementsByClassName('col-md-4')[0].getElementsByTagName('p')[0];
-            if (productDescription.innerHTML.length > 100) {
-                productDescription.innerHTML = productDescription.innerHTML.slice(0, 100) + '...';
-            }
-
-            currentRow.appendChild(products[0]);
+    for (let index = 0; index < numProducts; index++) {
+        if (index % 3 == 0) {
+            currentRow = document.createElement('DIV');
+            currentRow.className += 'row';
+            container.appendChild(currentRow);
         }
+
+        // Limit length of text in description on home page
+        let productDescription = products[0].getElementsByClassName('col-md-4')[0].getElementsByTagName('p')[0];
+        if (productDescription.innerHTML.length > 100) {
+            productDescription.innerHTML = productDescription.innerHTML.slice(0, 100) + '...';
+        }
+
+        currentRow.appendChild(products[0]);
     }
 }
