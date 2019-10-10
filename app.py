3102fb11d8cbc7f9d6cfce08606e67b4dac18952
RyanHopkins7 <ryan.hopkins@students.makeschool.com>
2019-10-09 17:26:45 -0700

    Display data from mongo

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ marketplace = db.marketplace
 
 @app.route('/')
 def index():
-    print(marketplace.find())
-    print(marketplace.find_one({'_id': ObjectId('5d9e013cf14aea0e5c07544d')}))
-    return render_template('index.html')
+    # print(marketplace.find())
+    # print(marketplace.find_one({'_id': ObjectId('5d9e013cf14aea0e5c07544d')})['title'])
+    return render_template('index.html', products=marketplace.find())
 
 @app.route('/items/new')
 def new_item():
